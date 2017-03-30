92034f4273c3105a0864b39d8ee7c9adb52972c3
martin52 <cmartin.andrade@gmail.com>
2017-03-30 13:19:02 -0300

    Agregada la sobrecarga al equals en ingrediente

--- src/main/java/tallerweb/sangucheto/modelo/Ingrediente.java
+++ src/main/java/tallerweb/sangucheto/modelo/Ingrediente.java
@@ -33,5 +33,29 @@ public class Ingrediente {
 	public String toString() {
 		return nombre + "       "+"precio=" + precio;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingrediente other = (Ingrediente) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+    
     
 }
